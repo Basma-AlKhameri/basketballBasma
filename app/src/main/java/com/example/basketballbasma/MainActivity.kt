38790751eb9a6562e57f901305ddbc278cf3757b
Basma-AlKhameri <basma21970@gmail.com>
2022-04-16 23:34:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/basketballbasma/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.basketballbasma
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.basketballbasma.ui.theme.BasketballBasmaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasketballBasmaTheme {
+                Main()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Main() {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baskball),
+            contentDescription = "basketball"
+        )
+        Box(Modifier.padding(30.dp)) {
+            Column() {
+                Text(
+                    text = "The Perfect Basketball Courts for you",
+                    //style = Typography.h1
+                )
+                Text(
+                    text = "Explore your dream basketball with" +
+                            " advanced basketball courts",
+                    //style = Typography.h3,
+                    modifier = Modifier.padding(top = 15.dp)
+                )
+            }//end of text box
+        }
+
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .padding(end = 40.dp),
+            contentAlignment = Alignment.BottomEnd
+        )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.ic_arr),
+                contentDescription = "arrow"
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/basketballbasma/ui/theme/Type.kt
@@ -0,0 +1,31 @@
+package com.example.basketballbasma.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.basketballbasma.R
+
+// Set of Material typography styles to start with
+val dosis = FontFamily(
+    Font(R.font.dosis_extra_bold,FontWeight.ExtraBold),
+    Font(R.font.dosis_regular, FontWeight.Normal)
+)
+
+val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = dosis,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 30.sp,
+        color = Color.Black
+    ),
+    h3 = TextStyle(
+        fontFamily = dosis,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        color = Color.LightGray
+    )
+)
